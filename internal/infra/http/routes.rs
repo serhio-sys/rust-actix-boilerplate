2d4cc9f0c298d1f11449b967d32a368afad53ac6
serhio-sys <serhii.kolodyazhniy@gmail.com>
2024-10-03 22:27:54 +0200

    Implemented cors and done some changes in routes

--- internal/infra/http/routes.rs
+++ internal/infra/http/routes.rs
@@ -11,6 +11,8 @@ use actix_web::{
 
 use crate::container::container::Container;
 
+const BASIC_PATH: &str = "/api/v1";
+
 use super::{
     controllers::{
         auth_controller::{ login, logout, register, AuthController },
@@ -22,8 +24,20 @@ use super::{
 pub fn init_routes(cfg: &mut web::ServiceConfig, container: Arc<Container>) {
     let auth_controller_data = web::Data::new(container.controllers.user_controller.clone());
     let user_controller_data = web::Data::new(container.controllers.auth_controller.clone());
-    init_user_routes(cfg, auth_controller_data, Arc::clone(&container));
-    init_auth_routes(cfg, user_controller_data, Arc::clone(&container));
+    cfg.service(
+        web
+            ::scope(BASIC_PATH)
+            .service(init_auth_routes(user_controller_data, Arc::clone(&container)))
+            .service(init_user_routes(auth_controller_data, Arc::clone(&container)))
+    );
+    cfg.service(
+        web::scope("/api").route(
+            "",
+            web::head().to(move || async move {
+                return HttpResponse::Ok().finish().map_into_boxed_body();
+            })
+        )
+    );
     cfg.default_service(web::get().to(not_found_handler));
 }
 
@@ -32,30 +46,42 @@ async fn not_found_handler() -> impl Responder {
 }
 
 fn init_auth_routes(
-    cfg: &mut web::ServiceConfig,
     auth_controller: Data<AuthController>,
     container: Arc<Container>
-) {
-    cfg.app_data(auth_controller.clone()).service(
-        web
-            ::scope("/auth")
-            .route("/register", web::post().to(register))
-            .route("/login", web::post().to(login))
-            .service(protected_route(container, "").route("/logout", web::post().to(logout)))
-    );
+) -> Scope<
+    impl ServiceFactory<
+        ServiceRequest,
+        Config = (),
+        Response = ServiceResponse,
+        Error = actix_web::Error,
+        InitError = ()
+    >
+> {
+    return web
+        ::scope("/auth")
+        .app_data(auth_controller.clone())
+        .route("/register", web::post().to(register))
+        .route("/login", web::post().to(login))
+        .service(protected_route(container, "").route("/logout", web::post().to(logout)));
 }
 
 fn init_user_routes(
-    cfg: &mut web::ServiceConfig,
     us_controller: Data<UserController>,
     container: Arc<Container>
-) {
-    cfg.app_data(us_controller.clone()).service(
-        protected_route(container, "/user")
-            .route("/all", web::get().to(find_all))
-            .route("", web::get().to(find_me))
-            .route("", web::delete().to(delete))
-    );
+) -> Scope<
+    impl ServiceFactory<
+        ServiceRequest,
+        Config = (),
+        Response = ServiceResponse,
+        Error = actix_web::Error,
+        InitError = ()
+    >
+> {
+    return protected_route(container, "/user")
+        .app_data(us_controller)
+        .route("/all", web::get().to(find_all))
+        .route("", web::get().to(find_me))
+        .route("", web::delete().to(delete));
 }
 
 fn protected_route(
--- internal/infra/http/server.rs
+++ internal/infra/http/server.rs
@@ -1,5 +1,6 @@
 use std::sync::Arc;
 
+use actix_cors::Cors;
 use actix_web::{ middleware::Logger, App, HttpServer };
 
 use crate::container::container::Container;
@@ -10,9 +11,17 @@ pub async fn start_server(container: Container) -> std::io::Result<()> {
     HttpServer::new(move || {
         let container_clone = Arc::new(container.clone());
         let logger = Logger::default();
-        App::new()
+        let cors = Cors::default()
+            .allowed_origin("https://*")
+            .allowed_origin("http://*")
+            .allowed_methods(["GET", "POST", "PUT", "DELETE", "OPTIONS"])
+            .allowed_headers(["Accept", "Authorization", "Content-Type", "X-CSRF-Token"])
+            .expose_headers(["Link"])
+            .max_age(300);
+        return App::new()
             .wrap(logger)
-            .configure(|cfg| routes::init_routes(cfg, container_clone.clone()))
+            .wrap(cors)
+            .configure(|cfg| routes::init_routes(cfg, container_clone.clone()));
     })
         .bind(("127.0.0.1", 8080))?
         .run().await
